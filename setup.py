de9b6910270108760d21f0c54bd5f85e0e0f779a
Ouadia Lasfar <ouadia.lasfar@gmail.com>
2021-03-11 18:05:38 +0100

    Update documentation

--- setup.py
+++ setup.py
@@ -13,55 +13,90 @@ from version import VERSION
 
 long_description = '''
 Amplitude Tracker
-==============
+=================
 
-Amplitude Tracker library lets you record analytics data from your Python code to [Amplitude](https://amplitude.com)
+Amplitude Tracker library lets you record analytics data from your
+Python code to `Amplitude`_
+
+Getting Started
+---------------
+
+Install ``amplitude-tracker`` using pip:
+
+::
+
+   pip install amplitude-tracker
+
+Inside your app, you’ll want to *set your* ``write_key`` before making
+any analytics calls:
+
+.. code:: python
+
+   import amplitude_tracker as amplitude
+
+   amplitude.write_key = 'xxxxxxxxxxxxxxx'
+
+*Note:* If you need to send data to multiple Segment sources, you can
+initialize a new Client for each write_key.
+
+Development Settings
+--------------------
+
+The default initialization settings are production-ready and queue
+messages to be processed by a background thread.
+
+In development you might want to enable some settings to make it easier
+to spot problems. Enabling amplitude.debug will log debugging info to
+the Python logger. You can also add an on_error handler to specifically
+print out the response you’re seeing from the Amplitude’s API.
+
+.. code:: python
+
+   def on_error(error, items):
+       print("An error occurred:", error)
 
 
-## Getting Started
+   analytics.debug = True
+   analytics.on_error = on_error
 
-Install `amplitude-tracker` using pip:
+Track
+-----
 
-```
-pip install amplitude-tracker
-```
+``track`` lets you record the actions your users perform. Every action
+triggers what we call an “event”, which can also have associated
+properties.
 
-Inside your app, you’ll want to *set your* `write_key` before making any analytics calls:
+.. code:: python
 
-```python
-import amplitude_tracker as amplitude
+   import amplitude_tracker as amplitude
+   amplitude.write_key = 'xxxxxxxxxxxxxxx'
 
-amplitude.write_key = 'xxxxxxxxxxxxxxx'
-```
-*Note:* If you need to send data to multiple Segment sources, you can initialize a new Client for each write_key.
+   amplitude.track(
+       user_id="xxx",
+       event_type="xxx",
+       user_properties={"trait": "xxx"},
+       event_properties={"attribute": "xxx"})
 
-## Development Settings
+Batching
+--------
 
-The default initialization settings are production-ready and queue messages to be processed by a background thread.
+This library is built to support high performance environments. That
+means it is safe to use amplitude-tracker on a web server that’s serving
+hundreds of requests per second.
 
-In development you might want to enable some settings to make it easier to spot problems. Enabling amplitude.debug will log debugging info to the Python logger. You can also add an on_error handler to specifically print out the response you’re seeing from the Amplitude's API.
+Every call ``track`` method *does not* result in an HTTP request, but is
+queued in memory instead. Messages are flushed in batch in the
+background, which allows for much faster operation.
 
+By default, this library will flush:
 
+-  every ``100`` messages (control with ``upload_size``)
+-  if ``0.5`` seconds has passed since the last flush (control with
+   ``upload_interval``)
 
-```python
-def on_error(error, items):
-    print("An error occurred:", error)
+There is a maximum of ``500KB`` per batch request and ``32KB`` per call.
 
-
-analytics.debug = True
-analytics.on_error = on_error
-```
-
-
-## Track
-
-`track` lets you record the actions your users perform. Every action triggers what we call an “event”, which can also have associated properties.
-
-```python
-import amplitude_tracker as amplitude
-amplitude.write_key = 'xxxxxxxxxxxxxxx'
-
-amplitude.track(
+.. _Amplitude: https://amplitude.com
 '''
 
 install_requires = [
